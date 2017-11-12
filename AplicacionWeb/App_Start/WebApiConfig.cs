4ebc073071a6311874b3ad80c00b44498ccf5f15
DanielCarbajal314 <daniel.carbajal@pucp.edu.pe>
2017-11-12 10:27:45 -0500

    Presentacion Web API

--- AplicacionWeb/App_Start/WebApiConfig.cs
+++ AplicacionWeb/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace AplicacionWeb
@@ -10,7 +11,8 @@ namespace AplicacionWeb
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            GlobalConfiguration.Configuration.Formatters.Clear();
+            GlobalConfiguration.Configuration.Formatters.Add(new JsonMediaTypeFormatter());
             // Web API routes
             config.MapHttpAttributeRoutes();
 
---
+++ AplicacionWeb/Controllers/API/ProductosController.cs
@@ -0,0 +1,31 @@
+﻿using GestionDeTienda;
+using InterfacesInventario.Productos;
+using InterfacesInventario.Productos.Peticiones;
+using InterfacesInventario.Productos.Respuestas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AplicacionWeb.Controllers.API
+{
+    public class ProductosController : ApiController
+    {
+
+        private IGestorDeProductos _gestorDeProductos = new GestorDeProductos();
+        // GET: api/Productos
+        public List<ProductoRegistrado> Get()
+        {
+            return _gestorDeProductos.ListarTodosLosProductos();
+        }
+
+
+        // POST: api/Productos
+        public void Post(NuevoProducto nuevoProducto)
+        {
+            _gestorDeProductos.CrearProducto(nuevoProducto);
+        }
+    }
+}
