a37bc3046aea6379896fb2d96bdefaf840f80f8d
DanielCarbajal314 <daniel.carbajal@pucp.edu.pe>
2017-11-12 09:35:08 -0500

    Presentacion Web

---
+++ AplicacionWeb/Controllers/ProductosController.cs
@@ -0,0 +1,21 @@
+﻿using GestionDeTienda;
+using InterfacesInventario.Productos;
+using InterfacesInventario.Productos.Respuestas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacionWeb.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private IGestorDeProductos _gestorDeProductos = new GestorDeProductos();
+        public ActionResult Index()
+        {
+            List<ProductoRegistrado> listaDeProductos = _gestorDeProductos.ListarTodosLosProductos();
+            return View(listaDeProductos);
+        }
+    }
+}
